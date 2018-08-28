8123467363bef8f6c3fc24bc848ebd5fa639356d
faganihajizada <hacizade.faqani@gmail.com>
2018-08-28 19:14:22 +0000

    hello 5

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World! - 4 auto docker"
+    return "Hello World! - 5 auto docker"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
