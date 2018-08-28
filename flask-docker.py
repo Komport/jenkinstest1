0d56adc2f5a45ea68e68e046b93169d1e5d2d908
faganihajizada <hacizade.faqani@gmail.com>
2018-08-28 20:33:43 +0000

    docker auto test 4

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World! - solved auto docker 3"
+    return "Hello World! - solved auto docker 4"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
