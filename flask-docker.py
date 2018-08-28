eabd174f30604adad774adbcce922b6ffe890064
faganihajizada <hacizade.faqani@gmail.com>
2018-08-28 18:40:04 +0000

    testjenkins1

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Changed it up a bit second try'
+    return 'Changed it up a bit new try'
 
 if __name__ == '__main__':
 app.run(debug=True,host='0.0.0.0')
