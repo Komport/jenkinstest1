3283c53cab5726d38cdf71b8dc54d3549a94619a
Yusif Yusifov <komport@gmail.com>
2018-08-29 09:43:10 +0400

    for loop added
    For loop

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World! - test for Yusif"
+    for i in range(0,10000000):
+        print(i)
+    #return "Hello World! - IBO SUCKS :D"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
