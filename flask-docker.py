35f8118192fd58223c47f8d5f498dd0f0f859c74
Yusif Yusifov <komport@gmail.com>
2018-08-29 09:48:23 +0400

    IBO sucks
    ibo sucks

--- flask-docker.py
+++ flask-docker.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    for i in range(0,10000000):
-        print(i)
-    #return "Hello World! - IBO SUCKS :D"
+    return "Hello World! - IBO SUCKS :D"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
