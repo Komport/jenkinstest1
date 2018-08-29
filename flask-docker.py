4bf4dfc3399fea8ed1480aa5a85712ef9b1d94b5
Yusif Yusifov <komport@gmail.com>
2018-08-29 09:33:00 +0400

    Test for Yusif
    It should be my test. Who is IBO?

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World! - test for Ibo"
+    return "Hello World! - test for Yusif"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
