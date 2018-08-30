2d12bfc0daf40dc17358222292daa29bd6088ef1
Fagani Hajizada <hacizade.faqani@gmail.com>
2018-08-30 21:30:49 +0400

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -9,6 +9,12 @@ font = Figlet(font="colossal")
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/templates")
+def hello(name):
+    return render_template(
+        'test.html',name=name)
  
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
