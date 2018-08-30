e46a4644e16ec15e17f45069383952038d80c011
Fagani Hajizada <hacizade.faqani@gmail.com>
2018-08-30 21:42:26 +0400

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 from pyfiglet import Figlet
 import os
 
@@ -9,6 +9,11 @@ font = Figlet(font="colossal")
 @app.route("/")
 def index():
     return "Flask App!"
+ 
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template(
+        'test.html',name=name)
  
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
