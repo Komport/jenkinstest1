70084ab0e08797962ce47b54c21acccc96cf34cc
Fagani Hajizada <hacizade.faqani@gmail.com>
2018-08-30 21:53:09 +0400

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -1,12 +1,11 @@
 from flask import Flask
- from pyfiglet import Figlet
- import os
- 
- app = Flask(__name__)
- 
-font = Figlet(font="colossal")
+from pyfiglet import Figlet
+import os
+app = Flask(__name__)
+font = Figlet(font="starwars")
 @app.route("/")
 def hello_world():
+    return "Hello World! - IBO SUCKS :D"
     message = os.getenv("MESSAGE", "no message specified")
     html_text = font.renderText(message)\
             .replace(" ","&nbsp;")\
@@ -14,8 +13,5 @@ def hello_world():
             .replace("<","&lt;")\
             .replace("\n","<br>")
     return "<html><body style='font-family: mono;'>" + html_text + "</body></html>"
-def hello():
-    return "Hello World!"
- 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
